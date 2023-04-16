36d523972774998aa3cc5af87f810685ae6feeb9
NURUZZAMAN <117540775+nzaman7878@users.noreply.github.com>
2023-04-17 02:18:50 +0530

    Add files via upload

---
+++ Event Listener Project/01 onclick/script.js
@@ -0,0 +1,8 @@
+// click event example
+
+const boxElement = document.getElementById("box");
+
+// add a click event listener to the element
+boxElement.addEventListener("click", function () {
+  alert("you just clicked me 👍");
+});
---
+++ Event Listener Project/02 doubleClick/script.js
@@ -0,0 +1,8 @@
+// dbclick event
+
+const boxElement = document.getElementById("box");
+
+// adding a dblclick event listener to the element
+boxElement.addEventListener("dblclick", function () {
+  alert("doubleClick event is working properly 👍");
+});
